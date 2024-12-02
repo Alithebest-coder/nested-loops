94f4c29b72c870fe7855dfa3aa89fb95a1ecd962
Alithebest-coder <aurumprint@proton.me>
2024-12-02 17:57:41 +1000

    Add files via upload

---
+++ nesterwhile.py
@@ -0,0 +1,9 @@
+name="abcdefggggggghiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiiigjkllllllllmnooooooooooooooooooooooopppppppppppppppppppppwqqqqqqqqqqwarrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrrsssssssssssssssyttttttttttttttttttttttttttttuuuuuuuuuuuuuuuuuuuuuuuuuuuuvvvvvvvvvvvvvvvvwwwwwwwwwwwwwwwwwwwwwwwxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxyyyyyyyyyyyyyyyyzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzzz"
+char=input("please enter char:   ")
+i=0
+count=0
+while i<len (name):
+    if name[i]==char:
+        count+=1
+    i+=1
+print(count)
